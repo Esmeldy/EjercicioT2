0799f104f6a390ea17cb5b30e7eedabddc8f3fcb
Esmeldy <esmeldyfm@hotmail.com>
2022-08-12 13:16:43 +0200

    Terminado el ejercicio

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Precio con IVA incluido: "+PrecioConIva(107));
     }
 
-
+    /**
+    *Para este ejercicio tendréis que crear una función que reciba un precio y devuelva el precio con el IVA incluido.
+    */
+    public static double PrecioConIva(double precio) {
+        double iva = 0.21;
+        double resultado = iva * precio;
+        return resultado + precio;
+    }
 }
